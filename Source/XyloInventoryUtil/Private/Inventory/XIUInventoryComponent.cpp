bc3bac28a4dcc375310c8ec7b3bc457fcf762ee7
XyloDatapacks <xylo.is.coding@gmail.com>
2024-08-18 17:37:15 +0200

    deleted inventory component functionality

--- Source/XyloInventoryUtil/Private/Inventory/XIUInventoryComponent.cpp
+++ Source/XyloInventoryUtil/Private/Inventory/XIUInventoryComponent.cpp
@@ -5,115 +5,13 @@
 
 #include "Inventory/XIUItemStack.h"
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
- * ItemStack
- */
-
-UXIUItemStack* FXIUInventorySlot::GetItemStack()
-{
-}
-
-void FXIUInventorySlot::SetItemStack(UXIUItemStack* ItemStack)
-{
-}
-
-UXIUItemStack* FXIUInventorySlot::GetFilterItem()
-{
-}
-
-void FXIUInventorySlot::SetFilterItem(UXIUItem* FilterItem)
-{
-}
-
-bool FXIUInventorySlot::GetShouldFilter()
-{
-}
-
-void FXIUInventorySlot::SetShouldFilter(bool bNewShouldFilter)
-{
-}
-
-bool FXIUInventorySlot::GetDisabled()
-{
-}
-
-void FXIUInventorySlot::SetDisabled(bool bNewDisabled)
-{
-}
-
-bool FXIUInventorySlot::IsFull()
-{
-}
-
-bool FXIUInventorySlot::CanInsert(UXIUItemStack* ItemStack)
-{
-	if (!ItemStack) return false;;
-	return !GetDisabled() && (!GetFilterItem() || (GetFilterItem()->GetItem() == ItemStack->GetItem() && !IsFull()));
-}
-
-bool FXIUInventorySlot::TryInsert(UXIUItemStack* ItemStack)
-{
-	if (CanInsert(ItemStack))
-	{
-		GetItemStack()->AddCount(ItemStack->GetCount()); // TODO: it has to return the count that did not fit into the stack (max count defined in Item)
-	}
-}
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
- * Inventory
- */
-
-FXIUInventory::FXIUInventory(int SlotsCount)
-{
-	Slots = TArray<FXIUInventorySlot*>(nullptr, SlotsCount);
-}
-
-bool FXIUInventory::AddItemStack(UXIUItemStack* ItemStack)
-{
-	TArray<FXIUInventorySlot*> AvailableSlots = Slots.FilterByPredicate([ItemStack](FXIUInventorySlot* Slot)
-	{
-		return Slot->CanInsert(ItemStack);
-	});
-	
-	for (FXIUInventorySlot* InventorySlot : AvailableSlots)
-	{
-		InventorySlot->TryInsert(ItemStack);
-		if (ItemStack->GetCount() == 0) break;
-	}
-}
-
-bool FXIUInventory::RemoveItemStack(UXIUItemStack* ItemStack, int Count)
-{
-	
-}
-
-FXIUInventorySlot* FXIUInventory::GetSlot(int Index)
-{
-}
-
-FXIUInventorySlot* FXIUInventory::GetSlot(UXIUItemStack* ItemStack)
-{
-}
-
-
-
-
-
-
-
-
-
-
 
 UXIUInventoryComponent::UXIUInventoryComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 	SetIsReplicatedByDefault(true);
+
+	//Inventory = FXIUInventory::FXIUInventory(3);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
